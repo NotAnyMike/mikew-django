6f68f4a405282e581cc4f3d8d99e565e9ed27f5e
Mike Woodcock <mwc1.0@hotmail.com>
2016-02-01 15:16:24 -0500

    Updating backbone web app

--- DjangoWebProject/settings.py
+++ DjangoWebProject/settings.py
@@ -4,6 +4,7 @@ Django settings for DjangoWebProject project.
 
 from os import path
 PROJECT_ROOT = path.dirname(path.abspath(path.dirname(__file__)))
+PROJECT_DIR = path.dirname(path.abspath(__file__))
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -70,7 +71,8 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = path.join(PROJECT_ROOT, 'static').replace('\\', '/')
+# STATIC_ROOT = path.join(PROJECT_ROOT, 'static').replace('\\', '/')
+STATIC_ROOT = path.join(PROJECT_DIR, '../static')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -129,7 +131,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    #'django.contrib.staticfiles',
     'app',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
@@ -168,3 +170,6 @@ LOGGING = {
 
 # Specify the default test runner.
 TEST_RUNNER = 'django.test.runner.DiscoverRunner'
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.core.context_processors.static',
+)
