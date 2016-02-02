bd191c640d158125667bc0ceff05200c79494df1
Mike Woodcock <mwc1.0@hotmail.com>
2016-02-02 14:05:15 -0500

    Addind default / url to point to index.html in urls.py

--- DjangoWebProject/urls.py
+++ DjangoWebProject/urls.py
@@ -6,12 +6,16 @@ from datetime import datetime
 from django.conf.urls import patterns, url
 from app.forms import BootstrapAuthenticationForm
 from DjangoWebProject.settings import STATIC_ROOT
+from django.views.generic.base import RedirectView
 
 # Uncomment the next lines to enable the admin:
 # from django.conf.urls import include
 # from django.contrib import admin
 # admin.autodiscover()
 
+roottoindex = "/index.html"
+
 urlpatterns = patterns('',
+	url(r'^$', RedirectView.as_view(url='/index.html')),
 	url(r'^(.*)$', 'django.views.static.serve', {'document_root': STATIC_ROOT, 'show_indexes' : True}),
 )
